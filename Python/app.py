38b8f02c2bfa17e4e53114ecc1e87080ce0b59db
H_abe <s2122072@stu.musashino-u.ac.jp>
2023-08-24 00:02:08 +0900

    delete chat.py

--- Python/app.py
+++ Python/app.py
@@ -2,6 +2,17 @@ import streamlit as st
 import openai
 import os
 from chat import ChatGPT
+def chat(text):
+    res = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role": "system",
+            "content": text
+        }
+        ]
+    )
+    return res["choices"][0]["message"]["content"]
 
 # 環境変数からAPIキーを設定
 openai_api_key = os.getenv("OPENAI_API_KEY")
@@ -12,6 +23,5 @@ user_input = st.text_input("質問を入力してください:")
 
 if user_input:
     st.write("考え中...")
-
-    answer = ChatGPT().chat(user_input)
+    answer = chat(user_input)
     st.write("回答:", answer)
